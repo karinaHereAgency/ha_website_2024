e4cfdd323464826f55a1a6102d39c3b8a40fa0e8
Karina <113564560+karinaHereAgency@users.noreply.github.com>
2024-06-19 14:40:12 +1000

    Update hospo_video_hover.js

--- hospo_video_hover.js
+++ hospo_video_hover.js
@@ -1,19 +1,16 @@
-jQuery(document).ready(function(){
+// Get All Videos
+const videos = document.querySelectorAll(" .hover_video_wrapper .video");
 
-    // Get All Videos
-    const videos = document.querySelectorAll(" .hover_video_wrapper .video");
-    const videos_boxes = jQuery(".hover_video_wrapper");
-    
-    //Loop over videos to add eventListeners
-    for (const videos_box of videos_boxes) {
-        videos_box.addEventListener('mouseover', function() { 
-            console.log('mouse entered');
-          videos_box.find(".video").play();
-        }, false);
-        
-        videos_box.addEventListener('mouseout', function() { 
-            console.log('mouse left');
-            videos_box.find(".video").pause();
-        }, false);
-    }
-});
+//Loop over videos to add eventListeners
+for (const video of videos) {
+    //Play on Mouseover
+    video.addEventListener('mouseover', function() { 
+      video.play();
+// Feel free to add functionalty here
+    }, false);
+  //Play on Mouseout
+    video.addEventListener('mouseout', function() { 
+// Feel free to add functionalty here
+video.pause();
+    }, false);
+}
